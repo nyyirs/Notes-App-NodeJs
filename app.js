8045d2ac23f47a5ffa0de65773d5733b1f403ec4
nyyirs <36409640+nyyirs@users.noreply.github.com>
2018-02-12 20:44:42 +0400

    Add Functionality
    Adding new TODO list completed

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const yargs = require ('yargs');
+const todo = require ('./todo.js')
 
+const argv = yargs.argv;
 const command = yargs.argv._[0];
 
 if (command === 'add'){
-    console.log('Add new TODO to list');
-};
-if (command === 'delete'){
+    
+    todo.addTodo(argv.title);
+
+} else if (command === 'delete'){
+
     console.log('Deleting a TODO');
-}
-if (command === 'list'){
+
+} else if (command === 'list'){
+
     console.log('Listing all TODOs');
-}else{
+
+} else{
+
     console.log('Enter Add, Delete or List only!');
+
 };
